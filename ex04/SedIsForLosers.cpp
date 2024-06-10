316fd2650186f32aaab967ba62c2af7d4b00a5a2
JIA0010 <HIROsroutine@gmail.com>
2024-06-10 13:06:02 +0900

    use my_replace

--- ex04/SedIsForLosers.cpp
+++ ex04/SedIsForLosers.cpp
@@ -2,17 +2,21 @@
 #include <iostream>
 #include <fstream>
 
+void SedIsForLosers::my_replace(std::string& str, size_t pos, size_t len, const std::string& newStr) {
+      str = str.substr(0, pos) + newStr + str.substr(pos + len);
+    }
+
 void SedIsForLosers::replace(const std::string& filename, const std::string& s1, const std::string& s2) {
     std::ifstream inputFile(filename);
     if (!inputFile.is_open()) {
-        std::cerr << "Error: Could not open the file " << filename << std::endl;
+        std::cout << "Error: Could not open the file " << filename << std::endl;
         return;
     }
 
     std::string outputFilename = filename + ".replace";
     std::ofstream outputFile(outputFilename);
     if (!outputFile.is_open()) {
-        std::cerr << "Error: Could not create the file " << outputFilename << std::endl;
+        std::cout << "Error: Could not create the file " << outputFilename << std::endl;
         inputFile.close();
         return;
     }
@@ -21,7 +25,7 @@ void SedIsForLosers::replace(const std::string& filename, const std::string& s1,
     while (std::getline(inputFile, line)) {
         size_t pos = 0;
         while ((pos = line.find(s1, pos)) != std::string::npos) {
-            line.replace(pos, s1.length(), s2);
+            my_replace(line, pos, s1.length(), s2);
             pos += s2.length();
         }
         outputFile << line << std::endl;
--- ex04/SedIsForLosers.hpp
+++ ex04/SedIsForLosers.hpp
@@ -4,6 +4,8 @@
 #include <string>
 
 class SedIsForLosers {
+private:
+    static void my_replace(std::string& str, size_t pos, size_t len, const std::string& newStr);
 public:
     static void replace(const std::string& filename, const std::string& s1, const std::string& s2);
 };
